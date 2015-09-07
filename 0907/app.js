2a5a48ffe82d1dc975f810bd6d0749c97d29a1fc
Kwag De Gi <tech@skyaperture.com>
2015-09-07 20:04:06 +0900

    anonymous function doesn't contain name property as expected

--- 0907/app.js
+++ 0907/app.js
@@ -13,3 +13,7 @@ function isNimble() { return true; }
 assert(typeof window.isNimble === 'function', "isNimble correctly defined");
 assert(isNimble.name === 'isNimble', "isNimble's name is isNimble");
 
+var canFly = function() { return true; }
+
+assert(typeof window.canFly === 'function', "canFly correctly defined in window scope");
+assert(canFly.name === '', "canFly doesn't contain name property");
