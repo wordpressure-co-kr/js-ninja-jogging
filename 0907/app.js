d36976a9fbf66f4720825160dd185c3a5a5c4941
Kwag De Gi <tech@skyaperture.com>
2015-09-07 20:00:52 +0900

    function's character - why we should test

--- 0907/app.js
+++ 0907/app.js
@@ -5,3 +5,11 @@ var text = "you can see this";
 function useless(callback) { return callback(); }
 
 assert(useless(function() { return text; }) === text, "Working..");
+
+
+
+function isNimble() { return true; }
+
+assert(typeof window.isNimble === 'function', "isNimble correctly defined");
+assert(isNimble.name === 'isNimble', "isNimble's name is isNimble");
+
